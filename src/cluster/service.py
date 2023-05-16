c523a97a86c56b3afbbb0dae980d1913e0ebf4d7
junhyeok <junwork@wisenut.co.kr>
2023-05-16 14:01:22 +0900

    revert: custom exception rollback

--- src/cluster/service.py
+++ src/cluster/service.py
@@ -19,56 +19,56 @@ class ClusterService:
         try:
             result = self.cluster_client.list_node()
             return response(result)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
     def get_pods(self, namespace: str = 'default'):
         try:
             result = self.cluster_client.list_namespaced_pod(namespace=namespace)
             return response(result)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
     def get_namespaces(self):
         try:
             result = self.cluster_client.list_namespace()
             return response(result, success_with_name_list)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
     def get_services(self, namespace: str = 'default'):
         try:
             result = self.cluster_client.list_namespaced_service(namespace=namespace)
             return response(result)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
     def get_secrets(self, namespace: str = 'default'):
         try:
             result = self.cluster_client.list_namespaced_secret(namespace=namespace)
             return response(result)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
     def get_volumes(self):
         try:
             result = self.cluster_client.list_persistent_volume()
             return response(result)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
     def get_volume_claims(self, namespace: str = 'default'):
         try:
             result = self.cluster_client.list_namespaced_persistent_volume_claim(namespace=namespace)
             return response(result)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
     def get_config_maps(self, namespace: str = 'default'):
         try:
             result = self.cluster_client.list_namespaced_config_map(namespace=namespace)
             return response(result)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
     def create_namespace(self, namespace: str):
@@ -77,14 +77,14 @@ class ClusterService:
             body.metadata = client.V1ObjectMeta(name=namespace)
             result = self.cluster_client.create_namespace(body=body)
             return response(result, success_with_no_content)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
     def delete_namespace(self, namespace: str):
         try:
             result = self.cluster_client.delete_namespace(name=namespace)
             return response(result, success_with_no_content)
-        except ClusterException as e:
+        except client.ApiException as e:
             return error_with_message(e)
 
 
