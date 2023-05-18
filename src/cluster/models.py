7913763d5ead5978438e14e8e2391cc4ade9154c
junhyeok <junwork@wisenut.co.kr>
2023-05-18 11:07:47 +0900

    refactor: metadata를 모델로 변환

--- src/cluster/models.py
+++ src/cluster/models.py
@@ -1,4 +1,15 @@
+
+
 from pydantic import BaseModel
+from pydantic.schema import datetime
+
+
+class Metadata(BaseModel):
+    name: str
+    create_date: datetime
+    annotations: dict | None
+    labels: dict | None
+    api_version: str | None
 
 
 class Volume(BaseModel):
@@ -16,3 +27,10 @@ class VolumeClaim(BaseModel):
     storage_class: str = 'default-storage-class'
     storage_size: str = '3Gi'
     access_mode: str = 'ReadWriteOnce'
+
+
+class ConfigMap(BaseModel):
+    name: str
+    data: dict = {}
+    labels: dict = {}
+    namespace: str = 'default'
