5342672b62710bda647d000bddd8d466001f7a75
hhhddd56 <hhhddd56@wisenut.co.kr>
2023-09-11 16:14:00 +0900

    :bug: pipeline_id 컬럼 타입 에러 수정

--- src/workflow_pipeline_module/models.py
+++ src/workflow_pipeline_module/models.py
@@ -1,15 +1,11 @@
-import uuid
-
 from sqlalchemy import Column, String, DateTime, Integer, JSON
 from sqlalchemy.orm import declarative_base
 
-
 Base = declarative_base()
-
 
 class Pipeline(Base):
     __tablename__ = "pipeline"
-    pipeline_id = Column(uuid, primary_key=True, index=True)
+    pipeline_id = Column(String, primary_key=True, index=True)
     pipeline_name = Column(String, index=True)
     pipeline_description = Column(String)
     version_name = Column(String)
